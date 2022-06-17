59840f25721a4ec43027043cc9987f6ac6b27bea
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-06-17 23:20:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,17 @@ data "ibm_container_cluster" "cluster" {
   cluster_name_id = "mycluster"
 }
 
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars = {
-    sample_var = var.sample_var
-  }
+provider "kubernetes" {
+  host                   = data.ibm_container_cluster_config.cluster_foo.host
 }
+
+# data "template_file" "test" {
+#   template = "Hello, I am a template. My sample_var value = $${sample_var}"
+
+#   vars = {
+#     sample_var = var.sample_var
+#   }
+# }
 
 # This is just to make sure that ibm provider is instantiated
 /* data "ibm_schematics_workspace" "test" {
@@ -17,12 +21,12 @@ data "template_file" "test" {
 }
 */
 
-resource "null_resource" "sleep" {
-  triggers = {
-    uuid = uuid()
-  }
+# resource "null_resource" "sleep" {
+#   triggers = {
+#     uuid = uuid()
+#   }
 
-  provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
-  }
-}
+#   provisioner "local-exec" {
+#     command = "sleep ${var.sleepy_time}"
+#   }
+# }
