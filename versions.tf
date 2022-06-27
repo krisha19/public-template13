9657ad26eb8519bc648365051a3cc8bf603dad87
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-06-27 12:51:50 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,24 @@
+# terraform {
+#   required_version = ">= 0.13"
+#   required_providers {
+#     null = {
+#       source = "hashicorp/null"
+#     }
+#     template = {
+#       source = "hashicorp/template"
+#     }
+#   }
+# }
+
 terraform {
-  required_version = ">= 0.13"
   required_providers {
-    null = {
-      source = "hashicorp/null"
-    }
-    template = {
-      source = "hashicorp/template"
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.21.1"
     }
   }
+}
+
+provider "ibm" {
+  
 }
